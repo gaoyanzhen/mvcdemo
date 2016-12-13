d61ec12ee17bdce6af830d292919bd5bbf2c154a
gaoyanzhen <g_y_zhen@163.com>
2016-12-13 18:04:30 +0800

    1.增加log4j日志打印 2.区分ContextLoaderListener与DispatcherServlet扫描组件，ContextLoaderListener不能扫描Controller组件

--- src/main/java/com/gyz/controller/LoginController.java
+++ src/main/java/com/gyz/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.gyz.controller;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -13,6 +15,7 @@ import com.gyz.service.IUserService;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+	private static Logger logger = LoggerFactory.getLogger(LoginController.class);
 	@Autowired
 	private IUserService userService;
 
@@ -25,7 +28,7 @@ public class LoginController {
 	
 	@RequestMapping(params = "action=login")
 	public String login(String username, String password, Map<String,Object> model) {
-
+		logger.debug(LoginController.class.toString());
 		// ÌáÊ¾ÐÅÏ¢
 		String message = "";
 		String view = "";
@@ -36,16 +39,18 @@ public class LoginController {
 
 		if (user != null && user.getPassword().equals(password)) {
 			message = "»¶Ó­µÇÂ¼ !";
+			logger.info("µÇÂ¼³É¹¦£¡");
 			view = "index";
 		}else{
 			message = "ÓÃ»§Ãû»òÃÜÂë´íÎó!";
+			logger.error("µÇÂ¼Ê§°Ü£¡");
 			view = "login";
 		}
 		model.put("message", message);
 		return view;
 	}
 
-	@RequestMapping(params = "action=register")
+	@RequestMapping(params = "register")
 	public String register(ModelMap model) {
 		return "register";
 	}
